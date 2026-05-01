5027ca75c01ca6e94cb61c58d0e6d856187ccf71
Allen Young <coffeevives888@gmail.com>
2026-04-30 19:02:57 -0600

    added facebook pixels

--- app/layout.tsx
+++ app/layout.tsx
@@ -103,6 +103,25 @@ export default function RootLayout({
             gtag('config', 'G-98GMCN3RXZ');
           `}
         </Script>
+
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+            document,'script','https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '964798809289904');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=964798809289904&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </head>
       <body className='bg-white text-black font-semibold flex flex-col min-h-screen overflow-x-hidden'>
         <SessionProviderWrapper>
